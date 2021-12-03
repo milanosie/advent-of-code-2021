f091fa17157476a1b1ad3ef47ae516397a9cc450
milansteenwinkel <milan.steenwinkel@incentro.com>
2021-12-03 11:35:00 +0100

    re-use

--- src/day3/index.ts
+++ src/day3/index.ts
@@ -27,33 +27,31 @@ const goB = (input) => {
   let co2 = '';
 
   for (let i = 0; i < numbers[0].length; i++) {
-    if(numbers.length > 1) {
-      let ones = 0;
-      let zeroes = 0;
-      numbers.forEach((v) => {
-        v.substring(i, i+1) == '0' ? zeroes++ : ones++;
-      });
-      zeroes > ones ? numbers = numbers.filter(n => n.substring(i, i + 1) === '0') : numbers = numbers.filter(n => n.substring(i, i + 1) === '1');
-    }
+    numbers = filterNumbers(numbers, i, false);
     if(numbers.length === 1) {
       oxygen = numbers[0];
     }
   }
   numbers = input.split(/\r?\n/).filter(n => n);
   for (let i = 0; i < numbers[0].length; i++) {
-    if(numbers.length > 1) {
-      let ones = 0;
-      let zeroes = 0;
-      numbers.forEach((v) => {
-        v.substring(i, i+1) == '0' ? zeroes++ : ones++;
-      });
-      zeroes > ones ? numbers = numbers.filter(n => n.substring(i, i + 1) === '1') : numbers = numbers.filter(n => n.substring(i, i + 1) === '0');
-    }
+    numbers = filterNumbers(numbers, i, true);
     if(numbers.length === 1) {
       co2 = numbers[0];
     }
   }
   return parseInt(oxygen, 2) * parseInt(co2, 2);
+}
+
+function filterNumbers(numbers: string[], i: number, one: boolean) {
+  if(numbers.length > 1) {
+    let ones = 0;
+    let zeroes = 0;
+    numbers.forEach((v) => {
+      v.substring(i, i+1) == '0' ? zeroes++ : ones++;
+    });
+    zeroes > ones ? numbers = numbers.filter(n => n.substring(i, i + 1) === (one ? '1' : '0')) : numbers = numbers.filter(n => n.substring(i, i + 1) === (one ? '0' : '1'));
+  }
+  return numbers;
 }
 
 
