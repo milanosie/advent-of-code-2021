cd928b252e901e4aeb9176961168042944b2dea6
milansteenwinkel <milan.steenwinkel@incentro.com>
2021-12-08 11:39:13 +0100

    Part 1 optimise

--- src/day8/index.ts
+++ src/day8/index.ts
@@ -5,39 +5,30 @@ const prepareInput = (rawInput: string) => rawInput
 const input = prepareInput(readInput())
 
 const searchOutput = (output: string) => {
-  // 1
-  let a = 0;
-  let b = 0;
-  let c = 0;
-  let d = 0;
-  let e = 0;
-  let f = 0;
-  let g = 0;
+  let a = output.match(/a/g)?.length > 0 ? 1 : 0
+  let b = output.match(/b/g)?.length > 0 ? 1 : 0
+  let c = output.match(/c/g)?.length > 0 ? 1 : 0
+  let d = output.match(/d/g)?.length > 0 ? 1 : 0
+  let e = output.match(/e/g)?.length > 0 ? 1 : 0
+  let f = output.match(/f/g)?.length > 0 ? 1 : 0
+  let g = output.match(/g/g)?.length > 0 ? 1 : 0
 
-  a = output.match(/a/g)?.length > 0 ? 1 : 0;
-  b = output.match(/b/g)?.length > 0 ? 1 : 0;
-  c = output.match(/c/g)?.length > 0 ? 1 : 0;
-  d = output.match(/d/g)?.length > 0 ? 1 : 0;
-  e = output.match(/e/g)?.length > 0 ? 1 : 0;
-  f = output.match(/f/g)?.length > 0 ? 1 : 0;
-  g = output.match(/g/g)?.length > 0 ? 1 : 0;
-
-  const sum  = a + b + c + d + e +f +g;
-  if(sum == 2 || sum == 4 || sum == 3 || sum == 7) return 1;
-  return 0;
+  const sum = a + b + c + d + e + f + g
+  if (sum == 2 || sum == 4 || sum == 3 || sum == 7) return 1
+  return 0
 }
 
 const goA = (input) => {
-  let segments = input.split(/\r?\n/).filter(n => n);
-  let count = 0;
+  let segments = input.split(/\r?\n/).filter(n => n)
+  let count = 0
   segments.forEach((segment) => {
-    segment = segment.split('|');
-    const segmentItems = segment[1].split(' ');
+    segment = segment.split("|")
+    const segmentItems = segment[1].split(" ")
     segmentItems.forEach((se) => {
-      count += searchOutput(se);
+      count += searchOutput(se)
     })
-  });
-  return count;
+  })
+  return count
 }
 
 const goB = (input) => {
