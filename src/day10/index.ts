c338c2eccaa40b8e9212dff8b35eb0329306ab67
milansteenwinkel <milan.steenwinkel@incentro.com>
2021-12-15 10:30:57 +0100

    Day 10 part 2

--- src/day10/index.ts
+++ src/day10/index.ts
@@ -2,67 +2,123 @@ import { test, readInput } from "../utils/index"
 
 const prepareInput = (rawInput: string) => rawInput
 
-const input = prepareInput(readInput()).split(/\r?\n/).filter(n => n);
+const input = prepareInput(readInput()).split(/\r?\n/).filter(n => n)
 
 function getSyntaxScore(input): number {
-  let chars = [];
-  let score = 0;
-  const inputArray = input.split('');
+  let chars = []
+  let score = 0
+  const inputArray = input.split("")
   inputArray.forEach((item) => {
-    if(score == 0) {
+    if (score == 0) {
       switch (item) {
-        case '(':
-          chars.push(item);
-          break;
-        case '[':
-          chars.push(item);
-          break;
-        case '{':
-          chars.push(item);
-          break;
-        case '<':
-          chars.push(item);
-          break;
-        case ')':
-          if(chars[chars.length - 1] != '(') {
-            score = 3;
+        case "(":
+          chars.push(item)
+          break
+        case "[":
+          chars.push(item)
+          break
+        case "{":
+          chars.push(item)
+          break
+        case "<":
+          chars.push(item)
+          break
+        case ")":
+          if (chars[chars.length - 1] != "(") {
+            score = 3
+          } else {
+            chars.splice(chars.length - 1, 1)
           }
+          break
+        case "]":
+          if (chars[chars.length - 1] != "[") score = 57
           else {
-            chars.splice(chars.length - 1, 1);
+            chars.splice(chars.length - 1, 1)
           }
-            break;
-        case ']':
-          if(chars[chars.length - 1] != '[') score = 57;
+          break
+        case "}":
+          if (chars[chars.length - 1] != "{") score = 1197
           else {
-            chars.splice(chars.length - 1, 1);
+            chars.splice(chars.length - 1, 1)
           }
-          break;
-        case '}':
-          if(chars[chars.length - 1] != '{') score = 1197;
+          break
+        case ">":
+          if (chars[chars.length - 1] != "<") score = 25137
           else {
-            chars.splice(chars.length - 1, 1);
+            chars.splice(chars.length - 1, 1)
           }
-          break;
-        case '>':
-          if(chars[chars.length - 1] != '<') score = 25137;
-          else {
-            chars.splice(chars.length - 1, 1);
-          }
-          break;
+          break
       }
     }
-  });
+  })
   return score
 }
 
 const goA = (input) => {
-  let score = 0;
-  input.forEach(i => score += getSyntaxScore(i));
-  return score;
+  let score = 0
+  input.forEach(i => score += getSyntaxScore(i))
+  return score
+}
+
+function completeLine(input): number {
+  let chars = []
+  let score = 0
+  const inputArray = input.split("")
+  inputArray.forEach((item) => {
+    switch (item) {
+      case "(":
+        chars.push(item)
+        break
+      case "[":
+        chars.push(item)
+        break
+      case "{":
+        chars.push(item)
+        break
+      case "<":
+        chars.push(item)
+        break
+      case ")":
+        chars.splice(chars.length - 1, 1)
+        break
+      case "]":
+        chars.splice(chars.length - 1, 1)
+        break
+      case "}":
+        chars.splice(chars.length - 1, 1)
+        break
+      case ">":
+        chars.splice(chars.length - 1, 1)
+        break
+    }
+  })
+
+  chars.reverse();
+  chars.forEach((c) => {
+    switch (c) {
+      case "(":
+        score = score * 5 + 1
+        break
+      case "[":
+        score = score * 5 + 2
+        break
+      case "{":
+        score = score * 5 + 3
+        break
+      case "<":
+        score = score * 5 + 4
+        break
+    }
+  })
+  return score
 }
 
 const goB = (input) => {
-  return
+  let scores = []
+  input = input.filter(i => getSyntaxScore(i) == 0)
+  input.forEach(i => scores.push(completeLine(i)))
+  scores.sort((a, b) => a - b)
+  return scores[scores.length / 2 - 0.5];
 }
 
 /* Tests */
